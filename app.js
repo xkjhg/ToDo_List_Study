e6c0f97442515d56503aa581e2ead3f606612dc0
xkjhg <xkjhg@naver.com>
2021-07-19 18:30:48 +0900

    third commit

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 const loginForm = document.querySelector("#login-form")
 const loginInput = document.querySelector("#login-form input")
+const greeting = document.querySelector("#greeting")
+
+const HIDDEN_CLASSNAME = "hidden"
+const USERNAME_KEY = "username"
 
 function onLoginSubmit(event) {
   event.preventDefault()
-  console.log(loginInput.value)
+  loginForm.classList.add(HIDDEN_CLASSNAME)
+  const username = loginInput.value
+  localStorage.setItem(USERNAME_KEY, username)
+  paintGreeting()
 }
 
-loginForm.addEventListener("submit", onLoginSubmit)
+function paintGreeting(username) {
+  greeting.innerHTML = `Hello ${username}`
+  greeting.classList.remove(HIDDEN_CLASSNAME)
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY)
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME)
+  loginForm.addEventListener("submit", onLoginSubmit)
+} else {
+  paintGreeting(savedUsername)
+}
