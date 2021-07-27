f8534866c8235ac96ab7995a92945f8f93f36a32
xkjhg <xkjhg@naver.com>
2021-07-27 17:50:22 +0900

    9th commit

--- JS/weather.js
+++ JS/weather.js
@@ -1,7 +1,16 @@
+const currentWeather = document.querySelector("#weather span:first-child")
+const currentCity = document.querySelector("#weather span:last-child")
+
+const API_KEY = config.WEATHER_API_KEY
+
 function onGeoSuccess(position) {
   const lat = position.coords.latitude
-  const long = position.coords.longitude
-  console.log(lat, long)
+  const lon = position.coords.longitude
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+  fetch(url).then(Response => Response.json()).then(data => {
+    currentWeather.innerText = `${data.weather[0].main} / ${data.main.temp}`
+    currentCity.innerText = data.name
+  })
 }
 
 function onGeoError() {
